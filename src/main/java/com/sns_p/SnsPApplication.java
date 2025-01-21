89b88a4efdf07ac5a01c68e2bc38486b969693b4
hyeonheee <nloavee@gmail.com>
2025-01-21 13:02:41 +0900

    Initial commit

---
+++ src/main/java/com/sns_p/SnsPApplication.java
@@ -0,0 +1,13 @@
+package com.sns_p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sns_p/SnsPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns_p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
